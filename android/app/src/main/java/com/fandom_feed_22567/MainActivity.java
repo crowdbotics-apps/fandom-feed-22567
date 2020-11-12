f55ab1076b8e0764d75dadd74d84445ad89a60ec
Crowdbotics <team@crowdbotics.com>
2020-11-12 21:21:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fandom_feed_22567/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fandom_feed_22567;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "fandom_feed_22567";
+    }
+}
